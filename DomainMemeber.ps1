a66f3fdaaba00f2a39fb658397aa2b4510ea920e
karker <said.karker@gmail.com>
2023-09-05 15:52:03 +0200

    DomainMember to Join Domain

---
+++ DomainMemeber.ps1
@@ -0,0 +1,17 @@
+param (
+    [Parameter(Mandatory=$true)][string]$domainName,
+    [Parameter(Mandatory=$true)][string]$adminUsername,
+    [Parameter(Mandatory=$true)][string]$AdminPassword
+)
+
+$DomainAdmin = $domainName + "\" + $adminUsername
+$Password = ConvertTo-SecureString -String $AdminPassword -AsPlainText -Force
+$DomainCredential = New-Object System.Management.Automation.PSCredential -ArgumentList $DomainAdmin, $Password
+
+$Param = @{
+    domainName = $domainName
+    adminUsername = $domainName + $adminUsername
+    AdminPassword = 'Password'
+}
+
+Add-Computer -DomainName $domainName -credential $DomainCredential -Restart
