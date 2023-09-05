8032cb0dbd648abe6e33440e4733f4ad6a519878
karker <said.karker@gmail.com>
2023-09-05 22:20:01 +0200

    added config for dc and one member

---
+++ dsc-config.ps1
@@ -0,0 +1,105 @@
+Configuration tenta4 {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$DomainName,
+        [Parameter(Mandatory = $true)]
+        [string]$AdminUsername,
+        [Parameter(Mandatory = $true)]
+        [string]$AdminPassword
+    )
+
+    $NetBiosName = $DomainName.Split(".")[0]
+    $DomainAdmin = $NetBiosName + "\" + $AdminUsername
+    $DomainAdminPassword = ConvertTo-SecureString -String $AdminPassword -AsPlainText -Force
+    $DomainAdminCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $DomainAdmin, $DomainAdminPassword
+
+    Import-DscResource -ModuleName xActiveDirectory -ModuleVersion "3.0.0.0"
+    Import-DscResource -ModuleName xNetworking -ModuleVersion "5.7.0.0"
+    Import-DscResource -ModuleName xDSCDomainjoin -ModuleVersion "1.2.23"
+    node dc01
+    {
+        # Install Active Directory Domain Services
+        WindowsFeature ADInstall {
+            Ensure               = "Present"
+            Name                 = "AD-Domain-Services"
+            IncludeAllSubFeature = $true
+        }
+        # Install Active Directory Domain Services Tools
+        WindowsFeature ADDSTools {
+            Ensure    = "Present"
+            Name      = "RSAT-ADDS"
+            DependsOn = "[WindowsFeature]ADInstall"
+
+        }
+        # Install Active Directory PowerShell Module
+        WindowsFeature "RSATADPowerShell" {
+            Ensure    = "Present"
+            Name      = "RSAT-AD-PowerShell"
+            DependsOn = "[WindowsFeature]ADDSTools"
+        }
+        # Install Active Directory Tools
+        WindowsFeature ActiveDirectoryTools {
+            Ensure    = "Present"
+            Name      = 'RSAT-AD-Tools'
+            DependsOn = "[WindowsFeature]ADDSTools"
+        }
+        # Install DNS Server Tools
+        WindowsFeature DNSServerTools {
+            Ensure    = "Present"
+            Name      = 'RSAT-DNS-Server'
+            DependsOn = "[WindowsFeature]ActiveDirectoryTools"
+        }
+        # Install DNS Server
+        WindowsFeature DNS {
+            Ensure    = "Present"
+            Name      = "DNS"
+            DependsOn = "[WindowsFeature]DNSServerTools"
+        }
+        # Promote server to Domain Controller
+        xADDomain Domain {
+            DomainName                    = $DomainName
+            DomainAdministratorCredential = $DomainAdminCredential
+            SafemodeAdministratorPassword = $DomainAdminCredential
+            DependsOn                     = "[WindowsFeature]ActiveDirectoryTools"
+        }
+    }
+
+    Node win01 {
+
+        xFirewall AllowPing {
+            Name        = "Allow Ping"
+            DisplayName = "Allow Ping"
+            Action      = "Allow"
+            Direction   = "Inbound"
+            Protocol    = "ICMPv4"
+        }
+
+        xFirewall AllowNetwrokDiscovery {
+            Name        = "Allow Netwrok Discovery"
+            DisplayName = "Allow Netwrok Discovery"
+            Action      = "Allow"
+            Direction   = "Inbound"
+            Protocol    = "UDP"
+            LocalPort   = "3702"
+            DependsOn   = "[xFirewall]AllowPing"
+        }
+        xDSCDomainjoin JoinDomain {
+            Domain     = $DomainName
+            Credential = $DomainAdminCredential
+            DependsOn  = "[xFirewall]AllowNetwrokDiscovery"
+        }
+
+    }
+}
+
+$ConfigData = @{
+    AllNodes = @(
+        @{
+            NodeName                    = "localhost";
+            PSDscAllowPlainTextPassword = $true;
+        }
+    )
+}
+
+tenta4 -ConfigurationData $ConfigData -OutputPath C:\DSCConfig
+Start-DscConfiguration -Path C:\DSCConfig -Wait -Verbose -Force
